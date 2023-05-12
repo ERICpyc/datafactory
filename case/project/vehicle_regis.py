4304d4e82970103a51ff8b664afd8cda3051fc68
“pengyc1” <pengyc1@xiaopeng.com>
2023-05-12 10:57:06 +0800

    iccid的return修改为字符串类型

--- case/project/vehicle_regis.py
+++ case/project/vehicle_regis.py
@@ -64,10 +64,10 @@ def vehicle_regis(vehicleTypeCode: str, vin= "", cduid= "", iccid=""):
             return ret2, ret3
         elif ret1.find("400") != -1:
             logger().warning("ICCID登记失败，ICCID已存在")
-            return {"code": 400, "message": "ICCID登记失败", "data": {"result": "ICCID登记失败，ICCID已存在，请联系管理员处理"}}
+
         else:
             logger().error("ICCID登记异常")
-            return {"code": 500, "message": "ICCID登记失败", "data": {"result": "ICCID登记异常，请联系管理员处理"}}
+            # return {"code": 500, "message": "ICCID登记失败", "data": {"result": "ICCID登记异常，请联系管理员处理"}}
 
 
 
