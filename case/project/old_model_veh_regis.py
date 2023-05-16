68bbc4e7eb1eb848167132d3b6dcf17ebe2bce92
“pengyc1” <pengyc1@xiaopeng.com>
2023-05-16 17:21:59 +0800

    bugfix

--- case/project/old_model_veh_regis.py
+++ case/project/old_model_veh_regis.py
@@ -2,7 +2,8 @@ from case.secens import tbox_cdu_bind, vehicle_bind
 from base.utiles import random_veh
 from base.utiles import logger
 
-def old_model_veh_regis(vehicleTypeCode= "", vin= "", cduid= "", iccid=""):
+
+def old_model_veh_regis(vehicleTypeCode="", vin="", cduid="", iccid=""):
     """
 	@api {post} /old_model_veh_regis D2X,E28,D55旧车型车辆登记
 	@apiName old_model_veh_regis
@@ -19,6 +20,7 @@ def old_model_veh_regis(vehicleTypeCode= "", vin= "", cduid= "", iccid=""):
     veh_info["iccid"] = iccid.strip()
     veh_info["vehicleTypeCode"] = vehicleTypeCode.strip()
     ran_value = random_veh()
+    old_vtype = ['DA', 'DB', 'DE', 'DC', 'DG', 'DF', 'ED']
     for key, value in zip(['vin', 'cduid', 'iccid'], ran_value):
         if not veh_info[key] or len(veh_info[key]) == 0:
             veh_info[key] = value
@@ -31,18 +33,23 @@ def old_model_veh_regis(vehicleTypeCode= "", vin= "", cduid= "", iccid=""):
         logger().warning("车型未填写")
         return {"code": 400, "message": "车型未填写,登记失败", "data": "车型必填，请检查车型是否填写正确！！"}
     else:
-        ret1 = tbox_cdu_bind.tbox_cdu_bind(cduid,iccid)
-        if ret1.get('code') == 200:
-            ret3 = vehicle_bind.vehicle_bind(iccid, cduid, vin, vehicleTypeCode)
-            return ret3
-        elif ret1.get('code') == 400:
-            logger().warning("大屏登记失败，ICCID已存在")
-            # return {"code": 400, "message": "ICCID登记失败", "data": "ICCID登记失败，ICCID:" + iccid + "已存在，请联系管理员处理"}
-            return ret1
+        if vehicleTypeCode in old_vtype:
+            ret1 = tbox_cdu_bind.tbox_cdu_bind(cduid, iccid)
+            if ret1.get('code') == 200:
+                ret3 = vehicle_bind.vehicle_bind(iccid, cduid, vin, vehicleTypeCode)
+                return ret3
+            elif ret1.get('code') == 400:
+                logger().warning("大屏登记失败，ICCID已存在")
+                # return {"code": 400, "message": "ICCID登记失败", "data": "ICCID登记失败，ICCID:" + iccid + "已存在，请联系管理员处理"}
+                return ret1
+            else:
+                logger().error("大屏绑定登记异常")
+                # return {"code": 500, "message": "ICCID登记失败", "data": "ICCID登记异常，请联系管理员处理"}
+                return ret1
         else:
-            logger().error("大屏绑定登记异常")
-            # return {"code": 500, "message": "ICCID登记失败", "data": "ICCID登记异常，请联系管理员处理"}
-            return ret1
+            logger().warning("车型不匹配")
+            return {"code": 400, "message": "车型未匹配,登记失败", "data": "车型必填DA、DB，DE,DC,DG,DF,ED之一"}
+
 
 if __name__ == "__main__":
     old_model_veh_regis(vehicleTypeCode='EA', vin='', cduid='XPENGF300000000000000004', iccid='')
