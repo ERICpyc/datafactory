878da6e948313f481d90234f31cf65204cb9f090
“pengyc1” <pengyc1@xiaopeng.com>
2023-06-30 15:17:36 +0800

    更新cookie_store

--- base/cookie_store.py
+++ base/cookie_store.py
@@ -1,7 +1,7 @@
-import sys
-from base.config import logger
+import os
 
-t_cookie = sys.argv[1]  # 获取第一个参数 t_cookie
-p_cookie = sys.argv[2]  # 获取第二个参数 p_cookie
+t_cookie = os.environ.get('T_COOKIE')
+p_cookie = os.environ.get('P_COOKIE')
+
 print(t_cookie)
 print(p_cookie)
