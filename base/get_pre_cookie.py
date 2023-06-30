ba527c3176a0c666c60f291609a564ef69c56cc2
“pengyc1” <pengyc1@xiaopeng.com>
2023-06-30 11:15:44 +0800

    get_cookie优化

--- base/get_pre_cookie.py
+++ base/get_pre_cookie.py
@@ -7,19 +7,21 @@ from selenium.webdriver.common.by import By
 
 def get_cookie():
     options = webdriver.ChromeOptions()
-    options.add_argument('--headless')
+    # options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
 
-    driver_path =  r'/usr/local/bin/chromedriver'
+    driver_path = r'/usr/local/bin/chromedriver'
     driver = webdriver.Chrome(driver_path, options=options)
 
     # 打开网页
 
-    driver.get("http://e.test.xiaopeng.local/#/login?forward_url=")
+    driver.get("https://e.deploy-test.xiaopeng.com/#/login?forward_url=")
     # 点击登录按钮
     driver.find_element(By.CSS_SELECTOR,
                         "#particles > div.login-wrapper > div > form > div:nth-child(1) > div > button").click()
+    driver.find_element(By.CSS_SELECTOR,
+                        "#__next > div > div.login_login_container__m0zeS > div.login_login_item__0Mprr.login_login_btn__nY2rY.login_login_btn_feishu__K3tFP > a").click()
     driver.implicitly_wait(10)
     driver.find_element(By.CSS_SELECTOR,
                         "#__next > div > div.login_login_container__m0zeS > form > div:nth-child(1) > div > input").send_keys(
