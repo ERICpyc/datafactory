ea22539b70aa36600fc9bab1da8e57c73a0f27b1
“pengyc1” <pengyc1@xiaopeng.com>
2023-07-25 16:10:56 +0800

    更新Cookie获取逻辑

--- base/config.py
+++ base/config.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 
 import logging,sys
-from base import cookie_store
-
 sys.path.append('/root/liwl5/FunDataFactory/data-factory-vehicle/base')
+
+
+from cookie_store import t_cookie,p_cookie
+
+
 def logger():
     logger = logging.getLogger()
     if len(logger.handlers) == 0:  # 只添加一个输出到控制台的handler
@@ -18,17 +21,17 @@ def logger():
     return logger
 
 
-if cookie_store.t_cookie:
+if t_cookie:
     # 使用 t_cookie 的值执行所需的操作
-    print("T_COOKIE:", cookie_store.t_cookie)
-    vmp_tcookie = cookie_store.t_cookie
+    print("T_COOKIE:", t_cookie)
+    vmp_tcookie = t_cookie
 else:
     print("未找到 T_COOKIE 参数")
 
-if cookie_store.p_cookie:
+if p_cookie:
     # 使用 p_cookie 的值执行所需的操作
-    print("P_COOKIE:", cookie_store.p_cookie)
-    vmp_pcookie = cookie_store.p_cookie
+    print("P_COOKIE:", p_cookie)
+    vmp_pcookie = p_cookie
 else:
     print("未找到 P_COOKIE 参数")
 
