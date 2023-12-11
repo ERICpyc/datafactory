57ce5398e9e51cd94434d98897f50c494de32aa7
“pengyc1” <pengyc1@xiaopeng.com>
2023-12-11 14:40:43 +0800

    优化UI自动化

--- base/get_pre_cookie.py
+++ base/get_pre_cookie.py
@@ -8,7 +8,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 options = webdriver.ChromeOptions()
-# options.add_argument('--headless')
+options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 driver_path = '/usr/local/bin/chromedriver'
@@ -17,39 +17,41 @@ driver = webdriver.Chrome(driver_path, options=options)
 
 
 def get_pre_cookie():
-    # 打开网页
-    driver.get("https://e.deploy-test.xiaopeng.com/#/login?forward_url=")
-    # 点击登录按钮
-    driver.implicitly_wait(10)
-    driver.find_element(By.CSS_SELECTOR,
-                        "#particles > div.login-wrapper > div > form > div:nth-child(1) > div > button").click()
-    # print(driver.page_source)
-    driver.implicitly_wait(10)
+    try:
+        # 打开网页
+        driver.get("https://e.deploy-test.xiaopeng.com/#/login?forward_url=")
+        # 点击登录按钮
+        driver.implicitly_wait(10)
+        driver.find_element(By.CSS_SELECTOR,
+                            "#particles > div.login-wrapper > div > form > div:nth-child(1) > div > button").click()
+        # print(driver.page_source)
+        driver.implicitly_wait(10)
 
-    # 预发布环境这个【其他登陆方式】时有时无的，再有问题要加下判断逻辑
-    driver.find_element(By.CSS_SELECTOR,
-                        "#__next > div > div.login_login_container__m0zeS > div.login_login_item__0Mprr.login_login_btn__nY2rY.login_login_btn_feishu__K3tFP > a").click()
-    driver.implicitly_wait(10)
-    driver.find_element(By.CSS_SELECTOR,
-                        "#__next > div > div.login_login_container__m0zeS > form > div:nth-child(1) > div > input").send_keys(
-        "test")
-    driver.find_element(By.CSS_SELECTOR,
-                        "#__next > div > div.login_login_container__m0zeS > form > div:nth-child(2) > div > input").send_keys(
-        "123456")
-    driver.find_element(By.CSS_SELECTOR,
-                        "#__next > div > div.login_login_container__m0zeS > div.login_login_item__0Mprr.login_login_btn__nY2rY > button").click()
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located(
-        (By.CSS_SELECTOR, "#app > div > section > div.topbar > div.platform-info-container > p")))
+        driver.find_element(By.CSS_SELECTOR,
+                            "#__next > div > div.login_login_container__m0zeS > div.login_login_item__0Mprr.login_login_btn__nY2rY.login_login_btn_feishu__K3tFP > a").click()
+        driver.implicitly_wait(10)
+        driver.find_element(By.CSS_SELECTOR,
+                            "#__next > div > div.login_login_container__m0zeS > form > div:nth-child(1) > div > input").send_keys(
+            "test")
+        driver.find_element(By.CSS_SELECTOR,
+                            "#__next > div > div.login_login_container__m0zeS > form > div:nth-child(2) > div > input").send_keys(
+            "123456")
+        driver.find_element(By.CSS_SELECTOR,
+                            "#__next > div > div.login_login_container__m0zeS > div.login_login_item__0Mprr.login_login_btn__nY2rY > button").click()
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located(
+            (By.CSS_SELECTOR, "#app > div > section > div.topbar > div.platform-info-container > p")))
 
-    cookie_dict = {}
-    # 获取cookie，并且按照键值对的格式存储起来
-    for c in driver.get_cookies():
-        cookie_dict[c['name']] = c['value']
+        cookie_dict = {}
+        # 获取cookie，并且按照键值对的格式存储起来
+        for c in driver.get_cookies():
+            cookie_dict[c['name']] = c['value']
 
-    # 将cookie转换为字符串，从字段里面拿键值，等号分割,每个键值对之间; 分割
-    str_cookie = ';'.join([k + '=' + v for k, v in cookie_dict.items()])
-    driver.quit()
-    return str_cookie
+        # 将cookie转换为字符串，从字段里面拿键值，等号分割,每个键值对之间; 分割
+        str_cookie = ';'.join([k + '=' + v for k, v in cookie_dict.items()])
+        driver.quit()
+        return str_cookie
+    except:
+        return "errormsg: unable to find elements,Check the Platform availability！"
 
 
 test_cookie = get_pre_cookie()
