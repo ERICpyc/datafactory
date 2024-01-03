e92b6c61c0ff997ad4c62d50c5b5655c0313935a
“pengyc1” <pengyc1@xiaopeng.com>
2024-01-03 09:45:10 +0800

    新增异常捕获，errmsg输出

--- case/scenes/tbox_regis.py
+++ case/scenes/tbox_regis.py
@@ -26,30 +26,32 @@ def tbox_regis(iccid,envoptions):
         "iccid": "{}".format(iccid),
         "partsNo": 121212
     }
-    try:
-        # 请求注册TBoX接口,并拿取响应结果
-        if envoptions.strip() == '2':
-            res = requests.post(url=url_test_iccid, json=body, headers=header)
-        else:
-            res = requests.post(url=url_iccid, json=body, headers=header)
-        # 转换为json格式p
-        res_json = res.json()
-        logger().info("输出注册TBOX接口响应数据：{}".format(res_json))
+    # 请求注册TBoX接口,并拿取响应结果
+    if envoptions.strip() == '2':
+        res = requests.post(url=url_test_iccid, json=body, headers=header)
+    else:
+        res = requests.post(url=url_iccid, json=body, headers=header)
+    # 转换为json格式p
+    res_json = res.json()
+    logger().info("输出注册TBOX接口响应数据：{}".format(res_json))
+
         # 断言走分支
-        try:
-            responseCode = res_json.get("code")
-            assert responseCode == 200
-            logger().info(f"---注册TBOX成功，输出断言结果：{iccid}！！！")
-            pil_bind.pil_bind(iccid)
-            return {"code": 200, "message": "ICCID登记成功", "data": "ICCID登记成功"}
-        except Exception as e:
-            responseCode = res_json.get("code")
-            assert responseCode == 400
-            logger().warning("TBOX注册失败，断言失败情况，TBOX已存在："+ iccid)
-            logger().warning("TBOX信息存在，跳过tbox注册！！！")
-            return {"code": 400, "message": "登记失败，iccid已存在", "data": "登记失败，iccid已存在", "errmsg":"{}".format(e)}
+    try:
+        responseCode = res_json.get("code")
+        assert responseCode == 200
+        logger().info(f"---注册TBOX成功，输出断言结果：{iccid}！！！")
+        pil_bind.pil_bind(iccid)
+        return {"code": responseCode, "message": "ICCID登记成功", "data": "ICCID登记成功"}
+    except AssertionError as e:
+        responseCode = res_json.get("code")
+        assert responseCode == 400
+        logger().warning("TBOX注册失败，断言失败情况，TBOX已存在："+ iccid)
+        logger().warning("TBOX信息存在，跳过tbox注册！！！")
+        return {"code": responseCode, "message": "登记失败，iccid已存在", "data": "登记失败，iccid已存在", "errmsg":"{}".format(e)}
     except Exception as e:
+        responseCode = res_json.get("code")
+        assert responseCode == 302 or 500
         logger().error("---！！注册修改TBOX都失败，输出异常信息：{}！！---".format(e))
         logger().error("---！！注册修改TBOX都失败，输出异常tbox：{}！！---".format(iccid))
-        return {"code": 500, "message": "登记失败，TBOX信息异常", "data": "登记失败，TBOX信息异常", "errmsg":"{}".format(e)}
+        return {"code": responseCode, "message": "登记失败，TBOX信息异常", "data": "登记失败，TBOX{}信息异常".format(iccid), "errmsg":"{}".format(e)}
 
